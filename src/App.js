6f2e6cdbe11a15204f6bf468c56cb1e0a73dbc19
Radoncode <relbouni69@gmail.com>
2023-09-24 18:20:18 +0100

    changing all my useState by one useReducer

--- src/App.js
+++ src/App.js
@@ -1,73 +1,122 @@
 import './App.css';
 import logo from './gallery-image-logo.svg';
 import Picture from './components/picture/picture.component';
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
+
+const PICTURE_ACTION_TYPES = {
+  'STORE_NAME_PICTURE': 'STORE_NAME_PICTURE',
+  'CHANGE_NAME_PICTURE': 'CHANGE_NAME_PICTURE',
+  'FETCH_PICTURES': 'FETCH_PICTURES',
+  'DISPLAY_PICTURES': 'DISPLAY_PICTURES',
+  'SELECT_PICTURE': 'SELECT_PICTURE',
+  'OPEN_MODAL': 'OPEN_MODAL'
+}
+
+const pictureReducer = (state, action) => {
+
+  const {type, payload} = action;
+
+  switch(type) {
+    case PICTURE_ACTION_TYPES.STORE_NAME_PICTURE:
+      return {
+        ...state,
+        search: state.search
+      };
+    case PICTURE_ACTION_TYPES.CHANGE_NAME_PICTURE:
+       return {
+        ...state,
+        search: payload
+      };
+    case PICTURE_ACTION_TYPES.FETCH_PICTURES:
+      return {
+        ...state,
+        search: '',
+        formData: payload
+      }
+    case PICTURE_ACTION_TYPES.DISPLAY_PICTURES:
+      return {
+        ...state,
+        images: payload
+      }
+    case PICTURE_ACTION_TYPES.SELECT_PICTURE:
+      return {
+        ...state,
+        selectedPicture: payload
+      }
+    case PICTURE_ACTION_TYPES.OPEN_MODAL:
+      return {
+        ...state,
+        openModal: payload
+      }
+    default:
+      throw new Error(`Unknown action ${type}`);
+  }
+}
+
+const INITIAL_STATE = {
+  search: '',
+  formData: '',
+  images: [],
+  selectedPicture: '',
+  openModal: false
+}
 
 function App() {
 
-  const [ search, setSearch] = useState('');
-  const [ formData, setFormData ] = useState('');
-  const [images, setImages] = useState([]);
-
-  const [selectedPicture, setSelectedPicture] = useState('');
-  const [openModal, setOpenModal] = useState(false);
-
-
+  const[ state, dispatch] = useReducer(pictureReducer, INITIAL_STATE);
    
   const onChangeHandler = (event) => {
-  const querySearch = event.target.value;
-  setSearch(querySearch);
+    dispatch({type: 'CHANGE_NAME_PICTURE', payload: event.target.value})
   }
   
   //when formData is updated , I start the useEffect function
   useEffect(() => {
-
     const fetchImages =  async () => {
-      const response = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${formData}&client_id=7JuVGIWoo9CP4LANK_gcp0FFVY06RrsAj-XK6QXNGYk`);
+      const response = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${state.formData}&client_id=7JuVGIWoo9CP4LANK_gcp0FFVY06RrsAj-XK6QXNGYk`);
       const images = await response.json();
       const datas = images.results;
-      setImages(datas)
+      dispatch({type: 'DISPLAY_PICTURES', payload: datas})
     }
     fetchImages();
 
-  }, [formData])
+  }, [state.formData])
 
 
   const onSubmitHandler =  (event) => {
     event.preventDefault();
-    setFormData(search);
-    setSearch('');  
+    dispatch({type: 'STORE_NAME_PICTURE'})
+    dispatch({type: 'FETCH_PICTURES', payload: state.search })
   }
 
   const onZoom = (picture) => {
-    setSelectedPicture(picture);
-    setOpenModal(true);
+    dispatch({type: 'SELECT_PICTURE', payload: picture})
+    dispatch({type: 'OPEN_MODAL', payload: true})
   }
 
   const inZoom = () => {
-    setSelectedPicture('');
-    setOpenModal(false);
+    dispatch({type: 'SELECT_PICTURE'})
+    dispatch({type: 'OPEN_MODAL', payload: false})
   }
   
   // create new array with only the Urls , I retrieve the index of the picture using useState 
   // for changing the state according to the new index
   const onMoveLeft = (picture) => {
-    const imagesUrls = images.map(image => image.urls.small);
+    const imagesUrls = state.images.map(image => image.urls.small);
     const indexUrlPicture = imagesUrls.indexOf(picture);
-    setSelectedPicture(imagesUrls[indexUrlPicture + 1]);
+    dispatch({type: 'SELECT_PICTURE', payload:imagesUrls[indexUrlPicture + 1]});
     if(typeof(imagesUrls[indexUrlPicture + 1]) === 'undefined'){
-      setSelectedPicture('');
-      setOpenModal(false);
+      dispatch({type: 'SELECT_PICTURE', payload: ''})
+      dispatch({type: 'OPEN_MODAL', payload: false})
     }
 
   }
   const onMoveRight = (picture) => {
-    const imagesUrls = images.map(image => image.urls.small);
+    const imagesUrls = state.images.map(image => image.urls.small);
     const indexUrlPicture = imagesUrls.indexOf(picture);
-    setSelectedPicture(imagesUrls[indexUrlPicture - 1]);
+    dispatch({type: 'SELECT_PICTURE', payload:imagesUrls[indexUrlPicture - 1]})
     if(typeof(imagesUrls[indexUrlPicture - 1]) === 'undefined'){
-      setSelectedPicture('');
-      setOpenModal(false);
+      dispatch({type: 'SELECT_PICTURE', payload: ''})
+      dispatch({type: 'OPEN_MODAL', payload: false})
     }
   }
 
@@ -83,7 +132,7 @@ function App() {
       <form className='columns is-centered' onSubmit={onSubmitHandler}>
           <div className="field is-grouped mt-6">
               <p className="control is-expanded">
-                <input className="input" type="text" placeholder="Find a photo" onChange={onChangeHandler} value={search}/>
+                <input className="input" type="text" placeholder="Find a photo" onChange={onChangeHandler} value={state.search}/>
               </p>
               <p className="control">
                 <button className="button is-primary">search</button>
@@ -92,7 +141,7 @@ function App() {
       </form>
       <div className="columns is-multiline is-mobile">     
       {
-        images && images.map(
+        state.images && state.images.map(
           (image) => <Picture 
                         key={image.id} 
                         picture={image.urls.small} 
@@ -100,12 +149,12 @@ function App() {
                       />)
       }
       </div>
-      <div className={`modal ${openModal ? 'is-active' : ''}`} >
+      <div className={`modal ${state.openModal ? 'is-active' : ''}`} >
         <div className="modal-background"></div>
           <div className="modal-content slide">
-              <div className="arrow left" onClick={() => onMoveLeft(selectedPicture)} />
-              <img className='image' src={selectedPicture} alt="" onClick={inZoom}/>
-              <div className="arrow right" onClick={() => onMoveRight(selectedPicture)}/>
+              <div className="arrow left" onClick={() => onMoveLeft(state.selectedPicture)} />
+              <img className='image' src={state.selectedPicture} alt="" onClick={inZoom}/>
+              <div className="arrow right" onClick={() => onMoveRight(state.selectedPicture)}/>
           </div>
         </div>
       </div>
